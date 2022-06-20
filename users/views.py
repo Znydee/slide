ab0496d166ba5e0f6f9fda4eb481f4ce0e819a3b
znydee <farouz98@gmail.com>
2022-06-20 15:10:00 +0100

    second commit

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def home(request):
+    return render(request,"users/index.html")
+
+def register(request):
     form=UserRegisterForm
-    return render(request,"users/index.html", {"form":form})
+    return render(request,"users/register.html", {"form":form})
