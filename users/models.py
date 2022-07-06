d1c6d09d2129e5b8e42f73dfdcbef4c4e49628a6
znydee <farouz98@gmail.com>
2022-07-06 22:30:30 +0100

    another commit

--- users/models.py
+++ users/models.py
@@ -30,3 +30,7 @@ class Comment(models.Model):
     comment_time = models.            DateTimeField(auto_now_add=True)
     def __str__(self):
         return f" {self.user.username} commented '{self.comment_made}' on {self.post}"
+        
+class Like(models.Model):
+    post=models.ForeignKey(Posts,on_delete=models.CASCADE,related_name="likes")
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="post_liked")
